3c3bf6a9817b98d98b72f6f99710c14c54868d6c
Sam Shull <sam.github@samshull.com>
2018-06-22 12:54:05 -0700

    Add every/some/attempt, update README, switch to assume (#2)

--- README.md
+++ README.md
@@ -3,4 +3,27 @@
 asynchronously iterate over items and resolve promises instead of callbacks
 
 This is the async library for promises, without a lot of extra
-dependencies.
+dependencies like in bluebird.
+
+### Methods
+
+  - `attempt(fn, ...args)` - Call a function with the given arguments and reject
+    caught errors
+  - `auto(tasks)` - Run tasks by specifying dependent additional dependent
+    tasks, resolves to an object containing the mapping of task property
+    names to task result values
+  - `delayed(fn, ms)` - Execute a function after a given number of milliseconds
+    and resolve
+  - `each(items, [limit,] iterator(item, index, array))` - Execute an iterator for a given set of items with a limit
+    on the number of simultaneous operations
+  - `every(items, [limit,] iterator(item, index, array))` - Resolve only if the iterator
+    resolves for each item
+  - `map(items, [limit,] iterator(item, index, array))` - Collect the results of all items
+    passed to the iterator
+  - `reduce(items, iterator(previous, item, index, array), accumulator = [])` - Sequentially call an
+    iterator for each item and include the previous result starting with
+    the value of the accumulator
+  - `some(items, [limit,] iterator(item, index, array))` - Resolve the first item that
+    resolves for the iterator, rejects if all reject
+  - `wrap(fn, ...args)` - Call a function using node callback style and
+    resolve or reject to a promise
--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,86 @@
 'use strict';
 
-const chai = require('chai');
+const assume = require('assume');
 const { stub } = require('sinon');
 
-const { expect } = chai;
+assume.use(require('assume-sinon'));
 
-chai.use(require('sinon-chai'));
-
-const { each, map, reduce, auto, wrap } = require('../');
+const { delayed, each, map, reduce, auto, wrap, attempt, some, every } = require('../');
 
 const delay = (ms = 10) => new Promise(a => setTimeout(a, ms));
 
 describe('promise helpers', function () {
+  describe('attempt', function () {
+    it('should catch and reject when the function throws', () => {
+      const error = new Error('rejected on purpose');
+      return attempt(() => { throw error; }).catch(e => assume(e).equals(error));
+    });
+
+    it('should resolve as expected for the arguments given', () => {
+      return attempt((a, b, c) => ({ a, b, c }), 1, 2, 3).then(r => assume(r).eqls({ a: 1, b: 2, c: 3 }));
+    });
+  });
+
+  describe('some', function () {
+    it('should reject if all items reject', () => {
+      return some([1, 2, 3, 4, 5], i => { throw new Error(`${i} error`); })
+        .catch(errors => {
+          assume(errors.length).equals(5);
+          assume(errors[0].message).equals('1 error');
+          assume(errors[1].message).equals('2 error');
+          assume(errors[2].message).equals('3 error');
+          assume(errors[3].message).equals('4 error');
+          assume(errors[4].message).equals('5 error');
+        });
+    });
+
+    it('should only process the limit of items simultaneously', () => {
+      const resolved = [];
+      return some([1, 2, 3, 4, 5], 3, i => {
+        if (i === 4) assume(resolved.length).gt(0);
+        if (i === 5) assume(resolved.length).gt(1);
+        return delay(i*100).then(() => { resolved.push(i); });
+      });
+    });
+
+    it('should resolve if any items resolve', () => {
+      return some([1, 2, 3, 4, 5], i => i).then(v => assume(v).equals(1));
+    });
+
+    it('should race to completion', () => {
+      return some([1, 2, 3, 4, 5], i => i === 3 ? i : delay(i*100)).then(v => assume(v).equals(3));
+    });
+  });
+
+  describe('every', function () {
+    it('should reject if any item rejects', () => {
+      const error = new Error('reject on purpose');
+      return every([1, 2, 3], i => { throw error; }).catch(e => assume(error).equals(e));
+    });
+
+    it('should only process the limit of items simultaneously', () => {
+      const resolved = [];
+      return every([1, 2, 3, 4, 5], 3, i => {
+        if (i === 4) assume(resolved.length).gt(0);
+        if (i === 5) assume(resolved.length).gt(1);
+        return delay(i*100).then(() => { resolved.push(i); });
+      });
+    });
+
+    it('should resolve if all items resolve', () => {
+      return every([1, 2, 3, 4, 5], i => i);
+    });
+  });
+
   describe('each', function () {
     it('should call the iterator for each item in the array', () => {
       const iterator = stub().resolves('yes');
       return each([1, 2, 3], 2, iterator)
         .then(() => {
-          expect(iterator).calledThrice;
-          expect(iterator).calledWith(1);
-          expect(iterator).calledWith(2);
-          expect(iterator).calledWith(3);
+          assume(iterator).called(3);
+          assume(iterator).calledWith(1);
+          assume(iterator).calledWith(2);
+          assume(iterator).calledWith(3);
         });
     });
 
@@ -28,10 +88,10 @@ describe('promise helpers', function () {
       const iterator = stub().resolves('yes');
       return each([1, 2, 3], iterator)
         .then(() => {
-          expect(iterator).calledThrice;
-          expect(iterator).calledWith(1);
-          expect(iterator).calledWith(2);
-          expect(iterator).calledWith(3);
+          assume(iterator).called(3);
+          assume(iterator).calledWith(1);
+          assume(iterator).calledWith(2);
+          assume(iterator).calledWith(3);
         });
     });
 
@@ -39,25 +99,25 @@ describe('promise helpers', function () {
       const resolved = {};
       const base = [1, 2, 3, 4, 5];
       return each(base, 2, function (arg, index, array) {
-        expect(array).equals(base);
-        expect(index).to.be.lessThan(array.length);
+        assume(array).equals(base);
+        assume(index).to.be.lessThan(array.length);
         if (arg === 3) {
-          expect(resolved[1]).to.equal(true);
-          expect(resolved[2]).to.be.undefined;
+          assume(resolved[1]).to.equal(true);
+          assume(resolved[2]).to.be.undefined;
         }
         if (arg === 5) {
-          expect(resolved[1] && resolved[2]).to.equal(true);
-          expect(resolved[3]).to.equal(true);
-          expect(resolved[4]).to.be.undefined;
+          assume(resolved[1] && resolved[2]).to.equal(true);
+          assume(resolved[3]).to.equal(true);
+          assume(resolved[4]).to.be.undefined;
         }
         return delay(10 * arg).then(() => { resolved[arg] = true; });
       })
         .then(() => {
-          expect(resolved[1]).to.equal(true);
-          expect(resolved[2]).to.equal(true);
-          expect(resolved[3]).to.equal(true);
-          expect(resolved[4]).to.equal(true);
-          expect(resolved[5]).to.equal(true);
+          assume(resolved[1]).to.equal(true);
+          assume(resolved[2]).to.equal(true);
+          assume(resolved[3]).to.equal(true);
+          assume(resolved[4]).to.equal(true);
+          assume(resolved[5]).to.equal(true);
         });
     });
   });
@@ -66,9 +126,9 @@ describe('promise helpers', function () {
     it('should collect the results in a new array', () => {
       return map([1, 2, 3, 4, 5], 3, a => a * 2)
         .then(result => {
-          expect(Array.isArray(result)).to.equal(true);
-          expect(result.length).to.equal(5);
-          expect(result).to.deep.equal([2, 4, 6, 8, 10]);
+          assume(Array.isArray(result)).to.equal(true);
+          assume(result.length).to.equal(5);
+          assume(result).to.deep.equal([2, 4, 6, 8, 10]);
         });
     });
 
@@ -76,10 +136,10 @@ describe('promise helpers', function () {
       const iterator = stub().resolves('yes');
       return map([1, 2, 3], iterator)
         .then(() => {
-          expect(iterator).calledThrice;
-          expect(iterator).calledWith(1);
-          expect(iterator).calledWith(2);
-          expect(iterator).calledWith(3);
+          assume(iterator).calledThrice;
+          assume(iterator).calledWith(1);
+          assume(iterator).calledWith(2);
+          assume(iterator).calledWith(3);
         });
     });
 
@@ -87,10 +147,10 @@ describe('promise helpers', function () {
       const iterator = stub().resolves('yes');
       return map([1, 2, 3], 2, iterator)
         .then(() => {
-          expect(iterator).calledThrice;
-          expect(iterator).calledWith(1);
-          expect(iterator).calledWith(2);
-          expect(iterator).calledWith(3);
+          assume(iterator).calledThrice;
+          assume(iterator).calledWith(1);
+          assume(iterator).calledWith(2);
+          assume(iterator).calledWith(3);
         });
     });
 
@@ -98,28 +158,28 @@ describe('promise helpers', function () {
       const resolved = {};
       const base = [1, 2, 3, 4, 5];
       return map(base, 2, function (arg, index, array) {
-        expect(array).equals(base);
-        expect(index).to.be.lessThan(array.length);
+        assume(array).equals(base);
+        assume(index).to.be.lessThan(array.length);
         if (arg === 3) {
-          expect(resolved[1]).to.equal(true);
-          expect(resolved[2]).to.be.undefined;
+          assume(resolved[1]).to.equal(true);
+          assume(resolved[2]).to.be.undefined;
         }
         if (arg === 5) {
-          expect(resolved[1] && resolved[2]).to.equal(true);
-          expect(resolved[3]).to.equal(true);
-          expect(resolved[4]).to.be.undefined;
+          assume(resolved[1] && resolved[2]).to.equal(true);
+          assume(resolved[3]).to.equal(true);
+          assume(resolved[4]).to.be.undefined;
         }
         return delay(10 * arg).then(() => { resolved[arg] = true; }).then(() => arg * 2);
       })
         .then(result => {
-          expect(resolved[1]).to.equal(true);
-          expect(resolved[2]).to.equal(true);
-          expect(resolved[3]).to.equal(true);
-          expect(resolved[4]).to.equal(true);
-          expect(resolved[5]).to.equal(true);
-          expect(Array.isArray(result)).to.equal(true);
-          expect(result.length).to.equal(5);
-          expect(result).to.deep.equal([2, 4, 6, 8, 10]);
+          assume(resolved[1]).to.equal(true);
+          assume(resolved[2]).to.equal(true);
+          assume(resolved[3]).to.equal(true);
+          assume(resolved[4]).to.equal(true);
+          assume(resolved[5]).to.equal(true);
+          assume(Array.isArray(result)).to.equal(true);
+          assume(result.length).to.equal(5);
+          assume(result).to.deep.equal([2, 4, 6, 8, 10]);
         });
     });
   });
@@ -130,9 +190,9 @@ describe('promise helpers', function () {
       const base = [1, 2, 3, 4, 5];
       const accumulator = [];
       return reduce(base, function (memo, arg, index, array) {
-        expect(memo).to.equal(accumulator);
-        expect(array).to.equal(base);
-        expect(index).to.be.lessThan(array.length);
+        assume(memo).to.equal(accumulator);
+        assume(array).to.equal(base);
+        assume(index).to.be.lessThan(array.length);
         return delay(10 * arg)
           .then(() => {
             resolved[arg] = true;
@@ -142,14 +202,14 @@ describe('promise helpers', function () {
           });
       }, accumulator)
         .then(result => {
-          expect(resolved[1]).to.equal(true);
-          expect(resolved[2]).to.equal(true);
-          expect(resolved[3]).to.equal(true);
-          expect(resolved[4]).to.equal(true);
-          expect(resolved[5]).to.equal(true);
-          expect(Array.isArray(result)).to.equal(true);
-          expect(result.length).to.equal(5);
-          expect(result).to.deep.equal([2, 4, 6, 8, 10]);
+          assume(resolved[1]).to.equal(true);
+          assume(resolved[2]).to.equal(true);
+          assume(resolved[3]).to.equal(true);
+          assume(resolved[4]).to.equal(true);
+          assume(resolved[5]).to.equal(true);
+          assume(Array.isArray(result)).to.equal(true);
+          assume(result.length).to.equal(5);
+          assume(result).to.deep.equal([2, 4, 6, 8, 10]);
         });
     });
   });
@@ -162,21 +222,21 @@ describe('promise helpers', function () {
         },
         second: ['first', ({ first, fourth }) => {
           // fourth depends on second and will not evaluate until second is resolved
-          expect(fourth).to.be.undefined;
-          expect(first).to.equal('first-done');
+          assume(fourth).to.be.undefined;
+          assume(first).to.equal('first-done');
           return 'second-done';
         }],
         third: ['fourth', ({ fourth }) => {
-          expect(fourth).to.equal('fourth-done');
+          assume(fourth).to.equal('fourth-done');
           return 'third-done';
         }],
         fourth: ['second', ({ second }) => {
-          expect(second).to.equal('second-done');
+          assume(second).to.equal('second-done');
           return Promise.resolve('fourth-done');
         }]
       }).then(result => {
-        expect(Object.keys(result)).to.deep.equal(['first', 'second', 'fourth', 'third']);
-        expect(Object.values(result)).to.deep.equal(['first-done', 'second-done', 'fourth-done', 'third-done']);
+        assume(Object.keys(result)).to.deep.equal(['first', 'second', 'fourth', 'third']);
+        assume(Object.values(result)).to.deep.equal(['first-done', 'second-done', 'fourth-done', 'third-done']);
       });
     });
   });
@@ -185,7 +245,7 @@ describe('promise helpers', function () {
     it('should wrap a callback function into a promise', () => {
       return wrap(cb => setTimeout(() => cb(null, 123), 10))
         .then(result => {
-          expect(result).to.equal(123);
+          assume(result).to.equal(123);
         });
     });
 
@@ -194,22 +254,8 @@ describe('promise helpers', function () {
         .then(() => {
           throw new Error('this should not have been called');
         }, err => {
-          expect(err.message).to.equal('expected');
+          assume(err.message).to.equal('expected');
         });
-    });
-
-    it('should call a finishing callback if passed', () => {
-      return wrap(cb => setTimeout(() => cb(null, 123), 10), (e, r) => {
-        expect(e).not.to.exist;
-        expect(r).to.equal(123);
-      });
-    });
-
-    it('should call a finishing callback with an error if passed', () => {
-      return wrap(cb => setTimeout(() => cb(new Error('expected')), 10), e => {
-        expect(e).to.exist;
-        expect(e.message).to.equal('expected');
-      });
     });
 
     it('should reject on error', () => {
@@ -217,7 +263,7 @@ describe('promise helpers', function () {
         .then(() => {
           throw new Error('this should not have been called');
         }, err => {
-          expect(err.message).to.equal('expected');
+          assume(err.message).to.equal('expected');
         });
     });
   });
