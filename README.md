3fb8014cbcc67607466b52275c4e38076df6b830
Sam Shull <sams@godaddy.com>
2019-02-17 18:29:20 -0800

    Version 2.0.0
    - Change error handling of `every` and `some` methods
    - Use `async`/`await`
    - Add `wait` method
    - Add `once` method
    - Update documentation

--- README.md
+++ README.md
@@ -7,23 +7,31 @@ dependencies like in bluebird.
 
 ### Methods
 
-  - `attempt(fn, ...args)` - Call a function with the given arguments and reject
+  - `attempt(fn:Function|AsyncFunction, ...args:Array<Any>):Promise<Any>` - Call a function with the given arguments and reject
     caught errors
-  - `auto(tasks)` - Run tasks by specifying dependent additional dependent
+  - `auto(tasks:Object<String, AsyncFunction|Array<String|AsyncFunction>>):Promise<Object>` - Run tasks by specifying dependent additional dependent
     tasks, resolves to an object containing the mapping of task property
-    names to task result values
-  - `delayed(fn, ms)` - Execute a function after a given number of milliseconds
-    and resolve
-  - `each(items, [limit,] iterator(item, index, array))` - Execute an iterator for a given set of items with a limit
+    names to task result values. See async.auto for how to construct `tasks`
+  - `delayed(fn:Function|AsyncFunction, ms:Number = 10):Promise<Any>` - Execute a function after a given number of milliseconds and resolve the promise to the function return value
+  - `each(items:Iterable, [limit:Number = POSITIVE_INFINITY,] iterator:Function(item:Any, index:Number, items:Iterable)):Promise` - Execute an iterator for a given set of items with a limit
     on the number of simultaneous operations
-  - `every(items, [limit,] iterator(item, index, array))` - Resolve only if the iterator
-    resolves for each item
-  - `map(items, [limit,] iterator(item, index, array))` - Collect the results of all items
+  - `every(items:Iterable, [limit:Number = POSITIVE_INFINITY,] iterator:Function(item:Any, index:Number, items:Iterable)):Promise<Boolean>` - Resolves true if all iterated items resolve true, otherwise resolves false
+  - `map(items:Iterable, [limit:Number = POSITIVE_INFINITY,] iterator:Function(item:Any, index:Number, items:Iterable))` - Collect the results of all items
     passed to the iterator
-  - `reduce(items, iterator(previous, item, index, array), accumulator = [])` - Sequentially call an
+  - `reduce(items:Iterable, iterator(previous:Any, item:Any, index:Number, items:Iterable), accumulator:Any)` - Sequentially call an
     iterator for each item and include the previous result starting with
     the value of the accumulator
-  - `some(items, [limit,] iterator(item, index, array))` - Resolve the first item that
-    resolves for the iterator, rejects if all reject
-  - `wrap(fn, ...args)` - Call a function using node callback style and
+  - `some(items:Iterable, [limit:Number = POSITIVE_INFINITY,] iterator:Function(item:Any, index:Number, items:Iterable)):Promise<Boolean>` - Resolves true if any iterated items resolve true, otherwise resolves false
+  - `wrap(fn:Function, ...args:Array<Any>):Promise<Any>` - Call a function using node callback style and
     resolve or reject to a promise
+  - `once(event:String|Array<String>, target:EventEmitter, timeout:Number = -1):Promise<Any>` - Wait for one or more events
+    on an EventEmitter and optionally timeout the wait
+  - `wait(ms:Number = 10)` - Use a promise for a setTimeout call
+
+
+### Changelog
+
+- v2.0.0
+* Use async / await where possible
+* Change behavior in every/some where errors would be swallowed
+* Add `once` method
--- index.js
+++ index.js
@@ -1,84 +1,82 @@
 'use strict';
 
-function delayed(fn, ms=10) {
+async function delayed(fn, ms = 10) {
+  await wait(ms);
+  return await fn();
+}
+
+async function wait(ms = 10) {
   const timer = accept => {
-    const t = setTimeout(accept, ms);
-    if (t.unref) return t.unref();
-    return t;
+    const time = setTimeout(accept, ms);
+    if (typeof time.unref === 'function') time.unref();
   };
-  return new Promise(timer).then(fn);
+  await new Promise(timer);
 }
 
-function attempt(fn, ...args) {
-  return new Promise(accept => accept(fn(...args)));
+async function attempt(fn, ...args) {
+  return await fn(...args);
 }
 
-function each(items, limit, iterator) {
+async function each(items, limit, iterator) {
   if (typeof limit === 'function') {
     [limit, iterator] = [Number.POSITIVE_INFINITY, limit];
   }
-  const length = items.length > limit ? limit : items.length;
-  let position = length - 1;
-  const end = items.length - 1;
-  const chainer = i => {
-    return attempt(iterator, items[i], i, items)
-      .then(() => {
-        return position < end && chainer(++position);
-      });
+  const parts = Array.from(items);
+  const length = parts.length > limit ? limit : parts.length;
+  let i = 0;
+  const chainer = async (position, item) => {
+    await attempt(iterator, item, position, items);
+    return parts.length && chainer(i++, parts.shift());
   };
-  return Promise.all(Array.from({ length }, (_, n) => chainer(n)));
+  const simultaneous = Array.from({ length }, (_, n) => chainer(i++, parts.shift()));
+  await Promise.all(simultaneous);
+  return items;
 }
 
-function map(items, limit, iterator) {
+async function map(items, limit, iterator) {
   if (typeof limit === 'function') {
     [limit, iterator] = [Number.POSITIVE_INFINITY, limit];
   }
-  const length = items.length > limit ? limit : items.length;
+  const parts = Array.from(items);
+  const length = parts.length > limit ? limit : parts.length;
   const results = [];
-  let position = length - 1;
-  const end = items.length - 1;
-  const chainer = i => {
-    return attempt(iterator, items[i], i, items)
-      .then(result => {
-        results[i] = result;
-        if (position < end) {
-          return chainer(++position);
-        }
-      });
+  let i = 0;
+  const chainer = async (position, item) => {
+    results[position] = await attempt(iterator, item, position, items);
+    return parts.length && chainer(i++, parts.shift());
   };
-  const promises = Array.from({ length }, (_, n) => chainer(n));
-  return Promise.all(promises).then(() => results);
+  const simultaneous = Array.from({ length }, () => chainer(i++, parts.shift()));
+  await Promise.all(simultaneous);
+  return results;
 }
 
-function reduce(items, iterator, accumulator = []) {
-  let position = 0;
-  const end = items.length - 1;
-  const chainer = (r, i) => {
-    return attempt(iterator, r, items[i], i, items)
-      .then(result => {
-        return position < end ? chainer(result, ++position) : result;
-      });
+async function reduce(items, iterator, accumulator) {
+  const parts = Array.from(items);
+  let i = 0;
+  const chainer = async (value, item, position) => {
+    const previous = await attempt(iterator, value, item, position, items);
+    return parts.length ? chainer(previous, parts.shift(), i++) : previous;
   };
-  return chainer(accumulator, position);
+  return await chainer(accumulator, parts.shift(), i++);
 }
 
-function auto(tasks) {
+async function auto(tasks) {
   const keys = Object.keys(tasks);
   const promises = {};
   const values = {};
-  const creator = key => {
+  const creator = async key => {
     if (promises[key]) return promises[key];
 
     const task = tasks[key];
 
     if (Array.isArray(task)) {
       promises[key] = task.reduce((promise, entry) => {
-        if (typeof entry === 'function')
-          return promise.then(() => entry(values));
-        return promise.then(() => creator(entry));
+        if (['string', 'symbol'].includes(typeof entry))
+          return promise.then(() => creator(entry));
+        return promise.then(() => entry(values));
       }, Promise.resolve());
     } else {
-      promises[key] = Promise.resolve(values).then(task);
+      promises[key] = attempt(task, values);
     }
 
     return promises[key]
@@ -87,71 +85,77 @@ function auto(tasks) {
         return value;
       });
   };
-  return Promise.all(keys.map(creator)).then(() => values);
+  await Promise.all(keys.map(creator));
+  return values;
 }
 
-function wrap(fn, ...args) {
-  return new Promise((accept, reject) => {
+async function wrap(fn, ...args) {
+  return await new Promise((accept, reject) => {
     fn(...args, (e, ...results) => {
       if (e) return void reject(e);
-      accept(...results);
+      if (results.length === 1) return accept(results[0]);
+      accept(results);
     });
   });
 }
 
-function some(items, limit, iterator) {
+async function some(items, limit, iterator) {
   if (typeof limit === 'function') {
     [limit, iterator] = [Number.POSITIVE_INFINITY, limit];
   }
-  const errors = [];
-  const length = items.length > limit ? limit : items.length;
-  let position = length - 1;
-  const end = items.length - 1;
+  const parts = Array.from(items);
+  const length = parts.length > limit ? limit : parts.length;
+  let i = 0;
+  let passed = false;
+  const chainer = async (position, item) => {
+    if (passed) return true;
+    passed = await attempt(iterator, item, position, items);
+    if (passed) return true;
+    if (parts.length) return chainer(i++, parts.shift());
+  };
+  const simultaneous = Array.from({ length }, () => chainer(i++, parts.shift()));
+  await Promise.race(simultaneous);
+  return !!passed;
+}
 
-  return new Promise((accept, reject) => {
-    let accepted = false;
-    const complete = v => {
-      if (accepted) return;
-      accepted = true;
-      accept(v);
-    };
-    const chainer = i => attempt(iterator, items[i], i, items).then(complete, (e) => {
-      errors[i] = e;
-      return position < end ? chainer(++position) : Promise.resolve();
-    });
-    Promise.all(Array.from({ length }, (_, n) => chainer(n))).then(() => {
-      if (accepted) return;
-      reject(errors);
-    });
+async function every(items, limit, iterator) {
+  if (typeof limit === 'function') {
+    [limit, iterator] = [Number.POSITIVE_INFINITY, limit];
+  }
+  const parts = Array.from(items);
+  const length = parts.length > limit ? limit : parts.length;
+  let i = 0;
+  let passing = true;
+  const chainer = async (position, item) => {
+    if (!passing) return;
+    const passed = await attempt(iterator, item, position, items);
+    passing = passing && !!passed;
+    if (passing && parts.length) return chainer(i++, parts.shift());
+  };
+  const simultaneous = Array.from({ length }, () => chainer(i++, parts.shift()));
+  await Promise.all(simultaneous);
+  return passing;
+}
+
+function on(target, event, callback) {
+  const on = target.addEventListener || target.addListener;
+  const off = target.removeEventListener || target.removeListener;
+  on.call(target, event, function handler(...args) {
+    off.call(target, event, handler);
+    if (args.length === 1) return void callback(args[0]);
+    callback(args);
   });
 }
 
-function every(items, limit, iterator) {
-  if (typeof limit === 'function') {
-    [limit, iterator] = [Number.POSITIVE_INFINITY, limit];
+async function once(events, target, timeout = -1) {
+  let result;
+  const simultaneous = [].concat(events).map(event => new Promise(accept => on(target, event, accept)));
+  if (timeout > -1) {
+    simultaneous.push(delayed(() => { if (!result) throw new Error('timeout'); }, timeout));
   }
-  const length = items.length > limit ? limit : items.length;
-  let position = length - 1;
-  const end = items.length - 1;
-
-  return new Promise((accept, reject) => {
-    let failed = false;
-    const fail = e => {
-      if (failed) return;
-      failed = true;
-      reject(e);
-    };
-    const next = () => {
-      return position < end && chainer(++position);
-    };
-    const chainer = i => {
-      return attempt(iterator, items[i], i, items).then(next, fail);
-    };
-    Promise.all(Array.from({ length }, (_, n) => chainer(n))).then(() => {
-      if (failed) return;
-      accept();
-    }, reject);
-  });
+  result = await Promise.race(simultaneous);
+  if (result instanceof Error) throw result;
+  return result;
 }
 
 module.exports = {
@@ -161,8 +165,10 @@ module.exports = {
   each,
   every,
   map,
+  once,
   reduce,
   some,
+  wait,
   wrap
 };
 
